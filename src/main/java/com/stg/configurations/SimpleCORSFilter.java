2768b74eda5fb8ab30754ce4251d8a6cfe8c647d
David <david@stgconsulting.com>
2016-10-10 14:16:48 -0600

    Temporarily added a wildcard for access

--- src/main/java/com/stg/configurations/SimpleCORSFilter.java
+++ src/main/java/com/stg/configurations/SimpleCORSFilter.java
@@ -35,7 +35,7 @@ public class SimpleCORSFilter implements Filter {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;
 
-		response.setHeader("Access-Control-Allow-Origin", "http://onboarding.stgconsulting.com https://onboarding.stgconsulting.com");
+		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
