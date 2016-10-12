b3067d83552b3520332b49fadd9c2bb3444a38e9
David Landes <david.landes@stgconsulting.com>
2016-10-11 21:39:03 -0600

    Beginnings of Oauth2

--- src/main/java/com/stg/OnboardingApplication.java
+++ src/main/java/com/stg/OnboardingApplication.java
@@ -2,14 +2,22 @@ package com.stg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@SpringBootApplication(exclude = { org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class })
+@SpringBootApplication
 @EnableScheduling
-// @EnableOAuth2Sso
-public class OnboardingApplication {
+//@EnableOAuth2Sso
+public class OnboardingApplication /*extends WebSecurityConfigurerAdapter*/ {
 
     public static void main(String[] args) {
 	SpringApplication.run(OnboardingApplication.class, args);
     }
+
+    /*@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	    http.antMatcher("/**").authorizeRequests().antMatchers("/", "/oauth2callback**", "/webjars/**").permitAll().anyRequest().authenticated();
+    }*/
 }
