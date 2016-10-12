018b0c49b0c78c734a130b8b2a612aa55e26dbf1
David <david@stgconsulting.com>
2016-10-12 09:34:31 -0600

    Fixed issue where we couldn't send PUT commands

--- src/main/java/com/stg/OnboardingApplication.java
+++ src/main/java/com/stg/OnboardingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class })
 @EnableScheduling
 //@EnableOAuth2Sso
 public class OnboardingApplication /*extends WebSecurityConfigurerAdapter*/ {
--- src/main/java/com/stg/models/Task.java
+++ src/main/java/com/stg/models/Task.java
@@ -4,20 +4,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 
 import org.springframework.data.rest.core.annotation.RestResource;
 
